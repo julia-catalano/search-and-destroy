8e6f02e5613c3b9eb572a45239490171832a8fd5
Martha Ivana <martha.syryca@gmail.com>
2019-09-05 15:41:59 -0400

    completed binary search EC

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,25 +1,44 @@
 'use strict';
 
 // Complete this algo
-const binarySearch = (array, target) => {
+const binarySearch = (array, target, startIdx) => {
 
-	const leftArray = array.slice(0, array.length/2)
-	const rightArray = array.slice(array.length/2)
+	// -----------------------------------------------
+	// SLICE SOLUTION
+	// -----------------------------------------------
+	// const leftArray = array.slice(0, array.length/2)
+	// const rightArray = array.slice(array.length/2)
 
-	if (target > rightArray[0]) {
-		for (let i = 0; i < rightArray.length; i++) {
-			if (rightArray[i] === target) {
-				return true
+	// if (target > rightArray[0]) {
+	// 	for (let i = 0; i < rightArray.length; i++) {
+	// 		if (rightArray[i] === target) {
+	// 			return true
+	// 		}
+	// 	}
+	// } else {
+	// 	for (let i = 0; i < leftArray.length; i++) {
+	// 		if (leftArray[i] === target) {
+	// 			return true
+	// 		}
+	// 	}
+	// }
+	// return false
+
+	// -----------------------------------------------
+	// STARTING INDEX SOLUTION (EXTRA CREDIT)
+	// -----------------------------------------------
+
+		if (array[startIdx] > target) {
+			for (let i = 0; i < startIdx; i++) {
+				if (array[i] === target) return true;
+			}
+		} else {
+			for (let i = startIdx; i < array.length; i++) {
+				if (array[i] === target) return true;
 			}
 		}
-	} else {
-		for (let i = 0; i < leftArray.length; i++) {
-			if (leftArray[i] === target) {
-				return true
-			}
-		}
-	}
-	return false
+
+	return false;
 };
 
 /*
@@ -28,6 +47,8 @@ const binarySearch = (array, target) => {
 	Can you augment the function above to run in constant O(1) space?
 	This means, you cannot edit/copy the original array!
 	How can we use other pieces of data, like pointers, to avoid slicing?
+
+	// SEE STARTING INDEX SOLUTION
 
 */
 
